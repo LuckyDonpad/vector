83f9ead1acb5aa798baaf7f983adbd61560662a1
Donpad <superlalko31@gmail.com>
2022-02-01 12:27:19 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/vector_lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_lab.dir/main.c.obj"
+  "libvector_lab.dll.a"
+  "vector_lab.exe"
+  "vector_lab.exe.manifest"
+  "vector_lab.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/vector_lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
