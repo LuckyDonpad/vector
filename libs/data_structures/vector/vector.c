8beec31a636503621716157c9ac3370f0d40c289
LuckyDonpad <superlalko31@gmail.com>
2022-02-01 14:08:09 +0300

    допилена функция createVector

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -4,10 +4,18 @@
 
 #include "vector.h"
 #include "malloc.h"
+#include <stdio.h.>
+#include <stdlib.h>
 
-vector createVector(size_t n){
-    int *data = (int *) malloc(sizeof(int) * n);
 
-    return (vector){data, 0, n};
+vector createVector(size_t n) {
+    if (n) {
+        int *data = (int *) malloc(sizeof(int) * n);
+        if (data == NULL) {
+            fprintf(stderr, "bad alloc ");
+            exit(1);
+        } else return (vector) {data, 0, n};
+    } else return (vector) {NULL, 0, 0};
 }
+
 
--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include "libs/data_structures/vector/vector.h"
+#include <stdint.h>
 
 int main() {
-    vector vector1 = createVector(10);
+    vector vector1 = createVector(SIZE_MAX);
     vector vector2 = createVector(7);
     vector1.size = 3;
     vector2.size = 42;
